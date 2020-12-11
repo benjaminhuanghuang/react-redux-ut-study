9c5c4daac6e8eeff6dca7df0c20396369fc70529
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-12-11 00:16:26 -0800

    update

--- src/pages/login/login.test.js
+++ src/pages/login/login.test.js
@@ -16,6 +16,7 @@ import React from 'react';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { mount } from 'enzyme';
+
 configure({ adapter: new Adapter() });
 
 const wrapper = mount(<Login />);
